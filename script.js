ec44d152ecc49c90c076fc8d9c52b3737c4ac85b
Vivek7548 <deshmukhvivek7548>
2023-11-06 18:48:28 +0530

    first commit

--- script.js
+++ script.js
@@ -1,64 +1,99 @@
-function displayItems() {
-    itemList.innerHTML = "";
-    const items = JSON.parse(localStorage.getItem("items")) || [];
+// const { default: axios } = require("axios");
 
+const itemForm = document.getElementById('itemForm');
+const itemList = document.getElementById('itemList');
+let items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem() {
+    const itemName = document.getElementById('itemName').value;
+    const itemDescription = document.getElementById('itemDescription').value;
+    const itemPrice = parseFloat(document.getElementById('itemPrice').value);
+    const itemQuantity = parseInt(document.getElementById('itemQuantity').value);
+
+    const newItem = {
+        name: itemName,
+        description: itemDescription,
+        price: itemPrice,
+        quantity: itemQuantity
+    };
+
+    items.push(newItem);
+    localStorage.setItem('items', JSON.stringify(items));
+    displayItems();
+}
+
+function displayItems() {
+    itemList.innerHTML = '';
     items.forEach(function(item, index) {
-        const itemDiv = document.createElement("div");
-        itemDiv.className = "item";
-        itemDiv.innerHTML = `
-            <strong>Name:</strong> ${item.name} <br>
-            <strong>Description:</strong> ${item.description} <br>
-            <strong>Price:</strong> $${item.price} <br>
-            <strong>Quantity:</strong> ${item.quantity} <br>
-            <button onclick="editItem(${index})">Edit</button>
-            <button onclick="deleteItem(${index})">Delete</button>
+        const itemElement = document.createElement('li');
+        itemElement.innerHTML = `
+            <strong>${item.name}</strong> - ${item.description} | Price: $${item.price.toFixed(2)} | Quantity: ${item.quantity}
             <button onclick="buyOne(${index})">Buy One</button>
             <button onclick="buyTwo(${index})">Buy Two</button>
             <button onclick="buyThree(${index})">Buy Three</button>
+            <button onclick="editItem(${index})">Edit</button>
+            <button onclick="deleteItem(${index})">Delete</button>
         `;
-        itemList.appendChild(itemDiv);
+        itemList.appendChild(itemElement);
     });
 }
 
-function buyOne(index) {
+function editItem(index) {
     const items = JSON.parse(localStorage.getItem("items")) || [];
+    const editedItem = items[index];
+    
+    
+    axios.put(`https://crudcrud.com/api/5e622e8e6c1d4e669cf6f633b6f5d763/apointdata/654737d92e0fb203e8543ab8`, editedItem)
+        .then(response => {
+            console.log('Item updated successfully:', response.data);
+            displayItems();
+        })
+        .catch(error => {
+            console.error('Error updating item:', error);
+        });
+}
+
+function buyOne(index) {
     if (items[index].quantity >= 1) {
         items[index].quantity -= 1;
-        localStorage.setItem("items", JSON.stringify(items));
+        localStorage.setItem('items', JSON.stringify(items));
         displayItems();
+    } else {
+        alert('Insufficient Quantity');
     }
 }
 
 function buyTwo(index) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
     if (items[index].quantity >= 2) {
         items[index].quantity -= 2;
-        localStorage.setItem("items", JSON.stringify(items));
+        localStorage.setItem('items', JSON.stringify(items));
         displayItems();
+    } else {
+        alert('Insufficient Quantity');
     }
 }
 
 function buyThree(index) {
-    const items = JSON.parse(localStorage.getItem("items")) || [];
     if (items[index].quantity >= 3) {
         items[index].quantity -= 3;
-        localStorage.setItem("items", JSON.stringify(items));
+        localStorage.setItem('items', JSON.stringify(items));
         displayItems();
+    } else {
+        alert('Insufficient Quantity');
     }
 }
-window.addEventListener("DOMContentLoaded", () => {
-    const data = axios.put("https://crudcrud.com/api/41fb4a53bec341e28e3332dc4d21f521")
-        .then((response) => {
 
-            for (var i = 0; i < response.data.length; i++) {
-                displayItems(response.data[i])
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    console.log(data)
-})
+function editItem(index) {
+    alert(`Editing item: ${items[index].name}`);
+}
 
-// Initial display of items
+function deleteItem(index) {
+    items.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(items));
+    displayItems();
+}
+
+
+
+
 displayItems();
